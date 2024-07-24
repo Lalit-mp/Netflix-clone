1230046354d405d65ee1e2153df4a3c4ab1f5b96
Lalit Mohan <111294030+Lalit-mp@users.noreply.github.com>
2024-07-24 16:04:14 +0530

    Update script.js
    all functions are working now

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const answers=  document.querySelectorAll(".q-container");
+import hindi from "./netflix-hindi-content.js";
+import english from "./netflix-english-content.js";
+import changeLanguage from "./change-lang.js"
+// Faq opener
+const answers = document.querySelectorAll(".q-container");
 
-for (let answer of answers){
-    const question = answer.children[0];
-    // console.log(question);
-    const dropdown= answer.children[0].children[1];
-    console.log(dropdown);
-    question.addEventListener("click",()=>{
-        const toggleAnswer= answer.children[1];
-        toggleAnswer.classList.toggle("ans-on");
-        dropdown.classList.toggle("rotated-dropdown")
-    })
+for (let answer of answers) {
+  const question = answer.children[0];
+  const dropdown = answer.children[0].children[1];
+  question.addEventListener("click", () => {
+    const toggleAnswer = answer.children[1];
+    toggleAnswer.classList.toggle("answer-active");
+    dropdown.classList.toggle("rotated-dropdown");
+  });
 }
 
-const langBtn= document.querySelectorAll(".main-lang-ctn")
-const languageButton= document.querySelectorAll(".lang-dropdown-list")
-console.log(languageButton);
-// console.log(languageButton);
-for (const langbtn of langBtn) {
-    langbtn.addEventListener("click",()=>{
+// laguage change button
+const langButton = document.querySelectorAll(".main-lang-ctn");
+for (const langbtn of langButton) {
+  const dropDownButton = document.getElementsByClassName("lang-dropdown-list");
+  langbtn.addEventListener("click",(e)=>{
+    for (const iterator of dropDownButton) {
+      iterator.classList.toggle("lang-dropdown-list-on");
+      if (e.target.textContent == "हिन्दी") {
+        langbtn.childNodes[1].children[1].textContent="हिन्दी";
+        changeLanguage("hindi");
+      }else if(e.target.textContent == "English") {
+        langbtn.childNodes[1].children[1].textContent="English";
+        changeLanguage("english");
+      }
+    }
 
-        for (const langButton of languageButton) {
-            langButton.classList.toggle("lang-on");
-            console.log("im working");
-            
-        }
     })
-}
+     
+};
